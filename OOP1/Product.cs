521cdba365f1d1126484652b509a2b246609fde8
Güldane Bedir <bedir0900@gmail.com>
2021-01-21 00:44:26 +0300

    21.01.2021

---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int UnitInStock { get; set; }
+
+        public int UnitPrice { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int Id { get; set; }
+    }
+}
